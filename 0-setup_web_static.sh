ece3ba6ad79d2ea863f5eb4f2cca864f2d7d7dd2
YousefShata <yossef_shata_a@yahoo.com>
2024-04-05 01:06:41 +0000

    work plz

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 #Install nignx
+
 sudo apt update
-sudo apt install nginx
-mkdir -p /data/web_static/shared/
-mkdir -p /data/web_static/releases/test/
+sudo apt install -y nginx
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
 html_content="<html>
 <head>
 <title>Fake HTML</title>
@@ -11,7 +14,7 @@ html_content="<html>
 <body><h1>Fake Hello World</h1></body>
 </html>"
 
-echo "$html_content" > /data/web_static/releases/test/index.html
+echo "$html_content" | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
 current_link="/data/web_static/current"
 
@@ -23,14 +26,11 @@ sudo ln -s /data/web_static/releases/test/ "$current_link"
 
 sudo chown -R ubuntu:ubuntu /data/
 
-echo 'server {
-        listen 80 default_server;
-        listen [::]:80;
+nginx_config="/etc/nginx/sites-available/default"
 
-        location /data/web_static/current/ {
-                alias hbnb_static;
-
-        }
-}' > /etc/nginx/sites-available/default
+echo "location /hbnb_static/ {
+    alias /data/web_static/current/;
+    index index.html index.htm;  # Add this line if necessary
+}" | sudo tee -a "$nginx_config" >/dev/null
 
 sudo service nginx restart
